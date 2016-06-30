2ee779f24cf0f7877bba62827e56fce3e54c2edd
Yulia Lushinskaya <ylu@portia.ru>
2016-06-30 23:25:55 +0300

    fix

--- index.php
+++ index.php
@@ -4,8 +4,5 @@
     $smarty = new Smarty();
 
     $isLogged = $_SESSION["is_logged"];
-    $pass = md5("12");
-    echo $pass;
-    echo "md5 = c20ad4d76fe97759aa27a0c99bff67";
     $smarty->assign("isLogged", $isLogged);
     $smarty->display("index.tpl");
