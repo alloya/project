b4528c77108bd1273c06dc1df9df7e0916464cb3
Yulia Lushinskaya <ylu@portia.ru>
2016-06-30 19:55:05 +0300

    safe change

--- book.php
+++ book.php
@@ -1,16 +1,21 @@
 <?php
 require_once("include/get_book.php");
 
+    $bookID = $_GET["bookID"];
+    $answerArray = getBook($bookID);
+    $title = $answerArray[0]['b_title'];
+    $author = $answerArray[0]['b_author'];
+    $descr = $answerArray[0]['b_description'];
+    $image = $answerArray[0]['b_image'];
 
-$bookID = $_GET["bookID"];
-
-$title = $answerArray[0]['b_title'];
-$author = $answerArray[0]['b_author'];
-$descr = $answerArray[0]['b_description'];
-$image = $answerArray[0]['b_image'];
-
-echo "$title </br>";
-echo $author;
-echo $descr;
-require_once "pages/book/book.html";
-?>
+    // echo "$title </br>";
+    // echo $author;
+    // echo $descr;
+    // echo $image;
+    $isLogged = $_SESSION["is_logged"];
+    $smarty->assign("title", $title);
+    $smarty->assign("author", $author);
+    $smarty->assign("descr", $descr);
+    $smarty->assign("image", $image);
+    $smarty->assign("isLogged", $isLogged);
+    $smarty->display("book.tpl");
